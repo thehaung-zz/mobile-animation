77443a9408d25987fcd4ab4a762fdd8d7037a608
Hau Nguyen <thehaung217@gmail.com>
2021-09-24 16:14:36 +0700

    fix: fix some change

--- app/src/main/java/com/example/myapplication/Activity2.java
+++ app/src/main/java/com/example/myapplication/Activity2.java
@@ -18,6 +18,8 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
         ImageView light_lavender = (ImageView) findViewById(R.id.imageView);
         ImageView lavender = (ImageView) findViewById(R.id.imageView2);
+        ImageView lavender_3 = (ImageView) findViewById(R.id.imageView7);
+        ImageView lavender_4 = (ImageView) findViewById(R.id.imageView8);
 
         Fade fade = new Fade();
         View decor = getWindow().getDecorView();
@@ -58,6 +60,50 @@ public class Activity2 extends AppCompatActivity {
                         ActivityOptionsCompat.makeSceneTransitionAnimation(
                                 Activity2.this, lavender,
                                 ViewCompat.getTransitionName(lavender));
+                // starting our activity with below method.
+
+                Bundle b = new Bundle();
+                b.putInt("imageId", R.drawable.lavender2);
+                b.putString("content", "Before the start of the fasting period of Lent (on Ash Wednesday), people had fun running Carnivals as it was their last chance until Easterto eat meat. The celebration was also a way to chase off the gloom of winter in the hope of spring.");
+                intent.putExtras(b);
+                startActivity(intent, options.toBundle());
+            }
+        });
+
+        lavender_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Activity2.this,
+                        Activity3.class);
+
+                ActivityOptionsCompat options =
+                        ActivityOptionsCompat.makeSceneTransitionAnimation(
+                                Activity2.this, lavender_3,
+                                ViewCompat.getTransitionName(lavender_3));
+
+
+                Bundle b = new Bundle();
+                b.putInt("imageId", R.drawable.lavender);
+                b.putString("content", "The word “carnival” derives from the Latin “carnelevare” meaning “to take out the meat”. The medieval Church banished meat from the table during the whole period of Lent, as it did with sugar, ingredients containing fat, eggs and dairy products. ");
+                intent.putExtras(b);
+                startActivity(intent, options.toBundle());
+            }
+        });
+
+        lavender_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on image click we are opening new activity
+                // and adding animation between this two activities.
+                Intent intent = new Intent(Activity2.this,
+                        Activity3.class);
+                // below method is used to make scene transition
+                // and adding fade animation in it.
+                ActivityOptionsCompat options =
+                        ActivityOptionsCompat.makeSceneTransitionAnimation(
+                                Activity2.this, lavender_4,
+                                ViewCompat.getTransitionName(lavender_4));
                 // starting our activity with below method.
 
                 Bundle b = new Bundle();
